0608ce382390096dee7812e55739940101836b06
Zeqi Wang <zeqi.wang@baruchmail.cuny.edu>
2018-05-13 22:21:32 -0400

    Update mean_var_monte.cpp

--- mean_var_monte.cpp
+++ mean_var_monte.cpp
@@ -6,8 +6,7 @@
 using namespace Eigen;
 using namespace std;
 
- // Here, we use Monte_Carlo simulations to evaluate utility function
- // we assume the utility function is u=1-exp(-b*x)
+ // Here, we use Monte_Carlo simulation to realize portfolio optimization
 
 float uniformRandom()
 {
@@ -26,20 +25,13 @@ RowVectorXf RandomW(int n){
   weight=weight/weight.sum();
   return weight;
   }
+//Here, we create a random row vector, [0,1] of each element, sum is 1
 
-int main(){
-  int n;
-  //the number of securities in our portfolio
-  cin>>n;
-  MatrixXf C(n,n);//convariance matrix
-  RowVectorXf w(n),u(n),m(n);
-  //vetcor w is the weight, and vector u is an identity vector;
-  //vector m is the expected return of each stock
-  //Here, we first choose three stocks to check our program
-  C<<0.1,0,0,0,0.5,0.3,0,0.3,0.2;
-  u<<1,1,1;
-  m<<0.3,0.5,0.7;
-  //
+RowVectorXf mean_var_monte(MatrixXf C, int n, RowVectorXf m){
+  //C is the convariance matrix
+  //n is the number of securities
+  //m is the expected return of each individual securities
+  RowVectorXf w(n);
   srand(time(NULL));
   w=RandomW(n);
   float variance;
@@ -55,6 +47,21 @@ int main(){
       variance=temp_variance;
     }
   }
+  return w;
+}
+
+int main(){
+  int n;
+  //the number of securities in our portfolio
+  cin>>n;
+  MatrixXf C(n,n);//convariance matrix
+  RowVectorXf w(n),m(n);
+  //vetcor w is the weight, and vector u is an identity vector;
+  //vector m is the expected return of each stock
+  //Here, we first choose three stocks to check our program
+  C<<0.1,0,0,0,0.5,0.3,0,0.3,0.2;
+  m<<0.3,0.5,0.7;
+  w=mean_var_monte(C,n,m);
   cout<<"The weight(without selling) of minimal variance is: "<<w<<endl;
   float port_return=w*m.transpose();
   cout<<"The expected return is: "<<port_return<<endl;
